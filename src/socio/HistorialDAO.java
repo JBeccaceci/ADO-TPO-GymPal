b2e3d9e8ed9aa4c2c540c9ac6363f3628a8ec34f
santim1996 <santim1996@hotmail.com>
2022-11-22 11:58:42 -0300

    SQL CONNECTOR

---
+++ src/socio/HistorialDAO.java
@@ -0,0 +1,20 @@
+import java.sql.SQLException;
+import java.util.List;
+  
+public interface HistorialDAO {
+
+    public int add(Historial hist)
+        throws SQLException;
+
+    public void delete(int id)
+        throws SQLException;
+
+    public Historial getHistorial(int id)
+        throws SQLException;
+
+    public List<Historial> getHistoriales()
+        throws SQLException;
+        
+    public void update(Historial hist)
+        throws SQLException;
+}
